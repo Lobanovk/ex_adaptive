e376e141c163ca2886ebd07788ed74df87663028
Lobanovk <38396223+Lobanovk@users.noreply.github.com>
2019-03-20 00:53:15 +0300

    --

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,92 +1,68 @@
-$('.owl-carousel1').owlCarousel({
+/**
+ * Rules for carousel
+ * @type Object {nav: boolean, dots: boolean, navClass: Array, items: number, margin: number, responsive: Object}
+ */
+const rules = {
   nav: true,
   dots: false,
-  navClass: [
-    'angle-left','angle-right'
-  ],
-  items: 1
-});
-$('.owl-carousel2').owlCarousel({
-  nav: true,
-  dots: false,
-  navContainerClass: 'owl-nav owl-nav2',
-  navClass: [
-    'angle-left','angle-right'
-  ],
-  items: 1
-});
-$('.owl-carousel3').owlCarousel({
-  nav: true,
-  dots: false,
-  loop: false,
-  navContainerClass: 'owl-nav owl-nav3',
-  navClass: [
-    'angle-left','angle-right'
-  ],
-  items: 3,
-  margin: -68,
-  responsive: {
-    1024: {
-      items: 3,
-      margin: -45
-    },
-    768: {
-      items: 3,
-      margin: -35
-    },
-    320: {
-      items: 1,
-    }
-  }
-});
-$('.owl-carousel4').owlCarousel({
-  nav: true,
-  dots: false,
-  navContainerClass: 'owl-nav owl-nav4',
-  navClass: [
-    'angle-left','angle-right'
-  ],
+  navClass: ["angle-left", "angle-right"],
   items: 1,
-  responsive: {
-    768: {
-      nav:true
-    },
-    320: {
-      nav: false
+  margin: null,
+  responsive: null
+};
+/**
+ * Carousel
+ */
+$('.owl-carousel').each(function () {
+  rules.items = 1;
+  rules.margin = null;
+  rules.responsive = null;
+  if($(this).data('slider') === 'slider3'){
+    rules.items = 3;
+    rules.margin = -68;
+    rules.responsive= {
+      1024: {
+        items: 3,
+        margin: -45
+      },
+      768: {
+        items: 3,
+        margin: -35
+      },
+      320: {
+        items: 1,
+      }
+    };
+  }
+  if($(this).data('slider') === 'slider4'){
+    rules.responsive= {
+      768: {
+        nav: true
+      },
+      320: {
+        nav: false
+      }
+    };
+  }
+  if($(this).data('slider') === 'slider5'){
+    rules.responsive= {
+      768: {
+        nav: true
+      },
+      320: {
+        items: 1,
+        nav: false
+      }
     }
   }
-});
-$('.owl-carousel5').owlCarousel({
-  nav: true,
-  dots: false,
-  navContainerClass: 'owl-nav owl-nav5',
-  navClass: [
-    'angle-left','angle-right'
-  ],
-  items: 1,
-  responsive: {
-    768: {
-      nav:true
-    },
-    320: {
-      items: 1,
-      nav: false
+  if($(this).data('slider') === 'slider6'){
+    rules.responsive={
+      320: {
+        items: 1
+      }
     }
   }
-});
-$('.owl-carousel6').owlCarousel({
-  nav: true,
-  dots: false,
-  navContainerClass: 'owl-nav owl-nav5',
-  navClass: [
-    'angle-left','angle-right'
-  ],
-  items: 1,
-  responsive: {
-    320: {
-      items: 1
-    }
-  }
+  $(this).owlCarousel($.extend({}, rules, $(this).data('params')));
 });
 
 $('<div>', {
